8084b934d9c82841183519e14814b36f1a6ff227
Mark K. Pettit <mark@pettit.org>
2015-02-09 00:50:29 -0800

    Remove redundant methods. Standardize use of _validate_coords. Prettier printing of board. Code cleanup.

--- Board.pm
+++ Board.pm
@@ -61,9 +61,7 @@ sub getLayout {
 sub setBoardPos {
   my ($self, $x, $y, $letter) = @_;
 
-  if ($x < 0 || $x > 4 || $y < 0 || $y > 4) {
-    croak "Invalid coords: ($x,$y)";
-  }
+  $self->_validate_coords($x, $y);
 
   my @layout = @{ $self->getLayout() };
   substr ($layout[$y], $x, 1, $letter);
@@ -77,19 +75,12 @@ sub getBoardPos {
   return substr($layout->[$y], $x, 1);
 }
 
-sub getIdAtPos {
-  my ($self, $x, $y) = @_;
-  return substr($self->{_layout}->[$y], $x, 1);
-}
-
 sub getNameAtPos {
   my ($self, $x, $y) = @_;
 
-  if ($x > 4 || $x < 0 || $y > 4 || $y < 0) {
-    croak "Illegal coordinate: ($x,$y)";
-  }
+  $self->_validate_coords($x, $y);
 
-  my $id = $self->getIdAtPos($x, $y);
+  my $id = $self->getBoardPos($x, $y);
   return $self->getOccupantNameById($id);
 }
 
@@ -98,17 +89,18 @@ sub willAnimalFit {
   my ($self, $animal, $x, $y) = @_;
 
   $self->_validate_coords($x, $y);
-  my $height = $animal->getHeight();
-  my $width = $animal->getWidth();
+  my $animal_height = $animal->getHeight();
+  my $animal_width = $animal->getWidth();
 
   # Is the animal too far down or to the right?
-  my $room_on_board = ((($x + $width) <= 5) && (($y + $height) <= 5));
+  my $room_on_board = ((($x + $animal_width)  <= 5)
+                    && (($y + $animal_height) <= 5));
   if (! $room_on_board) {
     return 0;
   }
 
-  for (my $y2 = 0; $y2 < $height; $y2++) {
-    for (my $x2 = 0; $x2 < $width; $x2++) {
+  for (my $y2 = 0; $y2 < $animal_height; $y2++) {
+    for (my $x2 = 0; $x2 < $animal_width; $x2++) {
       my $shape_char = $animal->getShapePos($x2, $y2);
       # Only test the board if the animal occupies that space
       next if $shape_char eq ' ';
@@ -133,8 +125,13 @@ sub getOccupantNameById {
   if ($id eq q{ }) {
     return 'empty';
   }
-  if ($id < 0 || $id > $self->getNumOccupants()) {
-    croak "No such occupant";
+  elsif ($id !~ /^\d$/) {
+    croak "Invalid id [$id]";
+  }
+
+  if ($id > $self->getNumOccupants()) {
+    my $num_occupants = $self->getNumOccupants();
+    croak "You asked for id [$id] but the max is $num_occupants";
   }
   for my $occ (@{ $self->{_occupants} }) {
     if ($occ->[0] == $id) {
@@ -161,7 +158,6 @@ sub insertAnimal {
     croak "Shape will not fit at ($x,$y).";
   }
 
-  my $shape = $animal->getShape();
   my $animal_id = $self->addOccupant($animal->getName());
 
   for (my $y2 = 0; $y2 < $animal->getHeight(); $y2++) {
@@ -179,9 +175,11 @@ sub printBoard {
   my ($self) = @_;
 
   my @layout = @{ $self->getLayout() };
+  print " -----\n";
   for my $line (@layout) {
     print "[$line]\n";
   }
+  print " -----\n";
   print "\n";
   for my $occupant (@{ $self->{_occupants} }) {
     printf "%1d: %s\n", $occupant->[0], $occupant->[1];
